a20f3da4d1ba4a7dd232e62966f0aa7bc4fccd6e
UNDERSAAR <alesalas1934@hotmail.com>
2024-03-07 01:32:36 -0500

    Animated mountain with GSAP

--- src/components/Mount/Mount.jsx
+++ src/components/Mount/Mount.jsx
@@ -1,13 +1,24 @@
-import React from 'react'
-import './mount.css'
-import msvg from '../../assets/Montañas.svg'
+import React, { useEffect } from 'react';
+import './mount.css';
+import msvg from '../../assets/Montañas.svg';
+import gsap from 'gsap';
 
 const Mount = () => {
+  useEffect(() => {
+    const animation = gsap.from('.mount', {
+      opacity: 0,
+      duration: 4,
+    });
+    return () => {
+      animation.revert();
+    };
+  }, []);
+  
   return (
     <div className="mount">
         <img src={msvg} alt="Montaña" />
     </div>
-  )
-}
+  );
+};
 
-export default Mount
+export default Mount;
