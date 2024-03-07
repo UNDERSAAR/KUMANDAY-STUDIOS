aee8d085f4fa953a47d9e03df0c5b53dd1c41717
UNDERSAAR <alesalas1934@hotmail.com>
2024-03-07 01:27:24 -0500

    Issue with framermotion on animated logo, svg pathLength

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,11 +1,27 @@
 import React, { useEffect } from 'react';
 import './navbar.css';
-import klogo from '../../assets/KumandayLogo.svg';
 import gsap from 'gsap';
+import { motion } from 'framer-motion';
+
+const pathVariants = {
+  hidden: {
+    opacity: 0,
+    pathLength: 0
+  },
+  visible: {
+    opacity: 1,
+    pathLength: 1,
+    transition: {
+      duration: 2,
+      ease: "easeInOut",
+      delay: 3
+    }
+  }
+}
 
 const Navbar = () => {
     useEffect(() => {
-      const items = Array.from(document.querySelectorAll('.subitems > a, .items > img'))
+      const items = Array.from(document.querySelectorAll('.subitems > a'))
       const animation = gsap.from(items, {
         y: -10,
         stagger: 0.4,
@@ -31,7 +47,15 @@ const Navbar = () => {
     <div className="navbar">
       <div className="line"></div>
       <div className="items">
-        <img src={klogo} alt="Kumanday Studios" />
+        <svg id="Capa_1" data-name="Capa 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 3000 2100.01">
+          <motion.path 
+            className="cls-1" 
+            d="M3000 2070.01 3000 2100.01 2820 2100.01 2100 930.01 2070 930.01 1590 1410.01 1590 2100.01 1409.99 2100.01 1409.99 1410 930 930.01 900 930.01 180 2100.01 0 2100.01 0 2070.01 780 810.01 780 780.01 30 30.01 30 .01 240 .01 1380 1140.01 1409.99 1140.01 1409.99 0 1590 0 1590 1140.01 1620 1140.01 2760 0 2970 0 2970 30 2220 780.01 2220 810.01 3000 2070.01z"
+            variants={pathVariants}
+            initial="hidden"
+            animate="visible"
+          />
+        </svg>
         <div className="subitems">
           <a href="#" >SOBRE NOSOTROS</a>
           <a href="#">PROYECTOS</a>
@@ -43,4 +67,4 @@ const Navbar = () => {
   );
 }
 
-export default Navbar
+export default Navbar;
