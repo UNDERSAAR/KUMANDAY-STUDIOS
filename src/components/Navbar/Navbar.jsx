f21fc948a1f5d83eaf2d774ca749e9cb1e43a7ff
UNDERSAAR <alesalas1934@hotmail.com>
2024-03-07 00:23:23 -0500

    Complete animation of navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,42 +1,46 @@
 import React, { useEffect } from 'react';
 import './navbar.css';
 import klogo from '../../assets/KumandayLogo.svg';
-import SplitType from 'split-type';
 import gsap from 'gsap';
 
 const Navbar = () => {
-  useEffect(() => {
-    const text1 = new SplitType("#text1");
-    const text2 = new SplitType("#text2");
-    const text3 = new SplitType("#text3");
-    const text4 = new SplitType("#text4");
+    useEffect(() => {
+      const items = Array.from(document.querySelectorAll('.subitems > a, .items > img'))
+      const animation = gsap.from(items, {
+        y: -10,
+        stagger: 0.4,
+        delay: 4,
+        duration: 1,
+        opacity: 0
+      });
 
-    const animation = gsap.from([text1.chars, text2.chars, text3.chars, text4.chars], {
-      y: -10,
-      stagger: 0.04,
-      delay: 4,
-      duration: .3,
-      opacity: 0
-    });
+      const animation2 = gsap.from('.line', {
+        scaleX: 0,
+        delay: 3.5,
+        duration: 1,
+        opacity: 0
+      })
 
-    return () => {
-      animation.kill(); // Kill the animation when component unmounts
-    };
-  }, []); // Run effect only once after initial render
+      return () => {
+        animation.revert();
+        animation2.revert();
+      };
+    }, []);
 
   return (
     <div className="navbar">
+      <div className="line"></div>
       <div className="items">
         <img src={klogo} alt="Kumanday Studios" />
         <div className="subitems">
-          <a href="#" id="text1" className="char">SOBRE NOSOTROS</a>
-          <a href="#" id="text2" className="char">PROYECTOS</a>
-          <a href="#" id="text3" className="char">CONTACTO</a>
-          <a href="#" id="text4" className="char">MENU</a>
+          <a href="#" >SOBRE NOSOTROS</a>
+          <a href="#">PROYECTOS</a>
+          <a href="#">CONTACTO</a>
+          <a href="#">MENU</a>
         </div>
       </div>
     </div>
   );
 }
 
-export default Navbar;
+export default Navbar
